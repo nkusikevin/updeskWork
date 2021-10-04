c8e414525bc3d78bb104707b3ac73eca94b6897d
“nkusikevin” <nkusikvnhart@gmail.com>
2021-10-04 23:38:32 +0200

    making new cahnges on cards

--- script.js
+++ script.js
@@ -9,26 +9,3 @@ function flip(event) {
 		}
 	}
 }
-
-let mainNavLinks = document.querySelectorAll("header nav ul li a");
-let mainSections = document.querySelectorAll("main section");
-console.log(mainNavLinks);
-let lastId;
-let cur = [];
-
-window.addEventListener("scroll", (event) => {
-	let fromTop = window.scrollY;
-
-	mainNavLinks.forEach((link) => {
-		let section = document.querySelector(link.hash);
-
-		if (
-			section.offsetTop <= fromTop &&
-			section.offsetTop + section.offsetHeight > fromTop
-		) {
-			link.classList.add("current");
-		} else {
-			link.classList.remove("current");
-		}
-	});
-});
